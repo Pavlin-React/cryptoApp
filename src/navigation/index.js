807480452ac7a01196e1215b6d0e0f41c2a15b52
Pavlin Ivanov <pavlinivanov9@gmail.com>
2022-01-02 14:09:52 +0200

    Bottom Tab Navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import CoinDetailScreen from '../screens/CoinDetailScreen';
-import HomeScreen from '../screens/HomeScreen';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CoinDetailScreen from "../screens/CoinDetailScreen";
+import HomeScreen from '../screens/HomeScreen/index'
+import BottomTabNavigator from '../navigation/BottomTabNavigator'
 
-let Stack = createNativeStackNavigator()
+let Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='Home' >
-      <Stack.Screen name='Home' component={HomeScreen} />
-      <Stack.Screen name='CoinDetailScreen' component={CoinDetailScreen} />
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName="Root"
+    >
+      <Stack.Screen name="Root" component={BottomTabNavigator} />
+      <Stack.Screen name="CoinDetailScreen" component={CoinDetailScreen} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
