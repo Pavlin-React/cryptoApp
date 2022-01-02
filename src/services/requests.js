474898ffe66c494687250ef571f003b7fb4583f4
Pavlin Ivanov <pavlinivanov9@gmail.com>
2022-01-02 21:18:35 +0200

    refreshControl

--- src/services/requests.js
+++ src/services/requests.js
@@ -21,3 +21,14 @@ export let getCoinMarketChart = async (coinId) => {
     console.log(e);
   }
 };
+
+export let getMarketData = async (pageNumber = 1) => {
+  try {
+    let response = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=${pageNumber}&sparkline=false&price_change_percentage=24h`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
